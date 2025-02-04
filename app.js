326738d204b6a7f8f51d480b803b19fb1c894b05
ClarqMT <CLARQ2@GMAIL.COM>
2025-02-04 00:25:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById("amigo").value;
+    
+
+        if (nuevoAmigo.trim() === ""){
+        document.getElementById("resultado").innerHTML = `No has ingresado datos aún`;
+        }else{
+        document.getElementById("resultado").innerHTML = "";
+        amigos.push(nuevoAmigo);
+        }
+    
+    document.getElementById("amigo").value = "";
+    document.getElementById("amigo").focus();
+    
+    mostrarAmigos();
+} 
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+    
+}
+function sortearAmigo() {
+
+    if (amigos.length === 0) {
+        document.getElementById("resultado").innerHTML = `No hay personas para realizar el sorteo`;
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `El amigo secreto es: ${amigoSorteado}`;
+}
